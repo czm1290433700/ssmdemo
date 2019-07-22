678d9512c53475226064136968dd81239ccc267d
czm <1290433700@qq.com>
2019-07-22 11:18:53 +0800

    增加phone和code的后台验证

---
+++ src/main/java/com/czm/controller/RegisterController.java
@@ -0,0 +1,59 @@
+package com.czm.controller;
+
+import com.czm.common.CodeCaptchaServlet;
+import com.czm.entity.User;
+import com.czm.service.UserService;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+public class RegisterController {
+    private final static Logger log = Logger.getLogger(RegisterController.class);
+    @Autowired
+    private UserService userService;
+    @RequestMapping("/checkPhone")
+    @ResponseBody
+    public Map<String, Object> checkPhone(Model model, @RequestParam(value = "phone", required = false) String phone) {
+        log.debug("注册-判断手机号" + phone + "是否可用");
+        Map map = new HashMap<String, Object>();
+        User user = userService.findByPhone(phone);
+        if (user == null) {
+            //未注册
+            map.put("message", "success");
+        } else {
+            //已注册
+            map.put("message", "fail");
+        }
+
+        return map;
+    }
+
+    @RequestMapping("/checkCode")
+    @ResponseBody
+    public Map<String, Object> checkCode(Model model, @RequestParam(value = "code", required = false) String code) {
+        log.debug("注册-判断验证码" + code + "是否可用");
+        Map map = new HashMap<String, Object>();
+        ServletRequestAttributes attrs = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        String vcode = (String) attrs.getRequest().getSession().getAttribute(CodeCaptchaServlet.VERCODE_KEY);
+
+        if (code.equals(vcode)) {
+            //验证码正确
+            map.put("message", "success");
+        } else {
+            //验证码错误
+            map.put("message", "fail");
+        }
+
+        return map;
+    }
+}
