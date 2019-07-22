e1182bfc94b1d43a3e2f4199216c7d381b3a2065
czm <1290433700@qq.com>
2019-07-22 11:40:44 +0800

    增加doRegister方法以及邮件支持

--- src/main/java/com/czm/controller/RegisterController.java
+++ src/main/java/com/czm/controller/RegisterController.java
@@ -1,10 +1,14 @@
 package com.czm.controller;
 
 import com.czm.common.CodeCaptchaServlet;
+import com.czm.common.MD5Util;
+import com.czm.common.SendEmail;
 import com.czm.entity.User;
 import com.czm.service.UserService;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +19,7 @@ import org.springframework.web.context.request.ServletRequestAttributes;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 @Controller
 public class RegisterController {
@@ -55,5 +60,72 @@ public class RegisterController {
         }
 
         return map;
+    }
+
+    @Autowired// redis数据库操作模板
+    private RedisTemplate<String, String> redisTemplate;
+    @RequestMapping("/doRegister")
+    public String doRegister(Model model, @RequestParam(value = "email", required = false) String email,
+                             @RequestParam(value = "password", required = false) String password,
+                             @RequestParam(value = "phone", required = false) String phone,
+                             @RequestParam(value = "nickName", required = false) String nickname,
+                             @RequestParam(value = "code", required = false) String code) {
+
+        log.debug("注册...");
+        if (StringUtils.isBlank(code)) {
+            model.addAttribute("error", "非法注册，请重新注册！");
+            return "../register";
+        }
+
+        int b = checkValidateCode(code);
+        if (b == -1) {
+            model.addAttribute("error", "验证码超时，请重新注册！");
+            return "../register";
+        } else if (b == 0) {
+            model.addAttribute("error", "验证码不正确,请重新输入!");
+            return "../register";
+        }
+
+
+        User user = userService.findByEmail(email);
+        if (user != null) {
+            model.addAttribute("error", "该用户已经被注册！");
+            return "../register";
+        } else {
+            user = new User();
+            user.setNickName(nickname);
+
+            user.setPassword(MD5Util.encodeToHex("salt"+password));
+            user.setPhone(phone);
+            user.setEmail(email);
+            user.setState("0");
+            user.setEnable("0");
+            user.setImgUrl("/images/icon_m.jpg");
+            //邮件激活码
+            String validateCode = MD5Util.encodeToHex("salt"+email + password);
+            redisTemplate.opsForValue().set(email, validateCode, 24, TimeUnit.HOURS);// 24小时 有效激活 redis保存激活码
+
+            userService.regist(user);
+
+            log.info("注册成功");
+            SendEmail.sendEmailMessage(email, validateCode);
+            String message = email + "," + validateCode;
+            model.addAttribute("message", message);
+            return "/regist/registerSuccess";
+
+        }
+    }
+    // 匹对验证码的正确性
+
+    public int checkValidateCode(String code) {
+        ServletRequestAttributes attrs = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        Object vercode = attrs.getRequest().getSession().getAttribute("VERCODE_KEY");
+        if (null == vercode) {
+            return -1;
+        }
+        if (!code.equalsIgnoreCase(vercode.toString())) {
+            return 0;
+        }
+        return 1;
     }
 }
